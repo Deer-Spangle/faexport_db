1f0b96b9a0a4b8e790c035d541daf4adb13486f4
Deer-Spangle <deer@spangle.org.uk>
2022-07-11 23:01:44 +0100

    Implementing User and UserSnapshot classes to fit the new write-focus model

--- faexport_db/models/user.py
+++ faexport_db/models/user.py
@@ -1,185 +1,145 @@
 import datetime
-from typing import Optional, Dict, Any, List, Tuple
+from typing import Optional, Dict, Any, List
 
-from psycopg2 import errors
-from psycopg2.errorcodes import UNIQUE_VIOLATION
-
-from faexport_db.db import merge_dicts, Database, json_to_db, UNSET, unset_to_null
+from faexport_db.db import merge_dicts, Database, json_to_db
+from faexport_db.models.archive_contributor import ArchiveContributor
 
 
 class User:
     def __init__(
             self,
-            user_id: int,
             website_id: str,
             site_user_id: str,
-            is_deleted: bool,
-            first_scanned: datetime.datetime,
-            latest_update: datetime.datetime,
-            display_name: Optional[str],
-            extra_data: Optional[Dict[str, Any]]
+            snapshots: List["UserSnapshot"],
     ):
-        self.user_id = user_id
         self.website_id = website_id
         self.site_user_id = site_user_id
-        self.is_deleted = is_deleted
-        self.first_scanned = first_scanned
-        self.latest_update = latest_update
-        self.display_name = display_name
-        self.extra_data = extra_data
-        self.updated = False
+        self.snapshots = snapshots
 
-    def add_update(self, update: "UserUpdate") -> None:
-        if update.update_time > self.latest_update:
-            self.updated = True
-            self.latest_update = update.update_time
-            if update.is_deleted is not UNSET:
-                self.is_deleted = update.is_deleted
-            if update.display_name is not UNSET:
-                self.display_name = update.display_name
-            if update.add_extra_data is not UNSET:
-                self.extra_data = merge_dicts(self.extra_data, update.add_extra_data)
-            return
-        # If it's an older update, we can still update some things
-        if update.add_extra_data is not UNSET:
-            new_extra_data = merge_dicts(update.add_extra_data, self.extra_data)
-            self.updated = self.updated or (self.extra_data != new_extra_data)
-            self.extra_data = new_extra_data
-        if self.display_name is None and update.display_name is not UNSET:
-            self.updated = self.updated or (self.display_name != update.display_name)
-            self.display_name = update.display_name
-        if self.first_scanned > update.update_time:
-            self.updated = True
-            self.first_scanned = update.update_time
+    @property
+    def sorted_snapshots(self) -> List["UserSnapshot"]:
+        return sorted(self.snapshots, key=lambda s: s.scan_datetime, reverse=True)
 
-    def save(self, db: "Database") -> None:
-        if self.updated:
-            db.update(
-                "UPDATE users "
-                "SET is_deleted = %s, first_scanned = %s, latest_update = %s, display_name = %s, extra_data = %s "
-                "WHERE user_id = %s", (
-                    self.is_deleted, self.first_scanned, self.latest_update, self.display_name,
-                    json_to_db(self.extra_data), self.user_id
-                )
-            )
+    @property
+    def is_deleted(self) -> bool:
+        return self.sorted_snapshots[0].is_deleted
 
-    @classmethod
-    def from_database_by_user_id(cls, db: "Database", user_id: int) -> Optional["User"]:
-        user_rows = db.select(
-            "SELECT user_id, website_id, site_user_id, is_deleted, first_scanned, latest_update, display_name, "
-            "extra_data "
-            "FROM users WHERE user_id = %s",
-            (user_id,),
-        )
-        return cls.from_user_rows(user_rows)
+    @property
+    def first_scanned(self) -> datetime.datetime:
+        return self.sorted_snapshots[-1].scan_datetime
+
+    @property
+    def latest_update(self) -> datetime.datetime:
+        return self.sorted_snapshots[0].scan_datetime
+
+    @property
+    def display_name(self) -> Optional[str]:
+        for snapshot in self.snapshots:
+            if snapshot.display_name is not None:
+                return snapshot.display_name
+        return None
+
+    @property
+    def extra_data(self) -> Dict[str, Any]:
+        extra_data = {}
+        for snapshot in self.sorted_snapshots[::-1]:
+            if snapshot.extra_data is not None:
+                extra_data = merge_dicts(extra_data, snapshot.extra_data)
+        return extra_data
 
     @classmethod
     def from_database(
         cls, db: "Database", website_id: str, site_user_id: str
     ) -> Optional["User"]:
-        user_rows = db.select(
-            "SELECT user_id, website_id, site_user_id, is_deleted, first_scanned, latest_update, display_name, "
-            "extra_data "
-            "FROM users WHERE website_id = %s AND site_user_id = %s",
-            (website_id, site_user_id),
+        snapshot_rows = db.select(
+            "SELECT u.user_snapshot_id, u.scan_datetime, u.archive_contributor_id, a.name as contributor_name, "
+            "u.ingest_datetime, u.is_deleted, u.display_name, u.extra_data "
+            "FROM user_snapshots u "
+            "LEFT JOIN archive_contributors a ON u.archive_contributor_id = a.contributor_id "
+            "WHERE website_id = %s AND site_user_id = %s",
+            (
+                website_id, site_user_id
+            )
         )
-        return cls.from_user_rows(user_rows)
-
-    @classmethod
-    def from_user_rows(
-        cls,
-        user_rows: List[
-            Tuple[
-                int,
-                str,
-                str,
-                bool,
-                datetime.datetime,
-                datetime.datetime,
-                Optional[str],
-                Optional[Dict],
-            ]
-        ],
-    ) -> Optional["User"]:
-        if not user_rows:
+        snapshots = []
+        contributors = {}
+        for row in snapshot_rows:
+            (
+                snapshot_id, scan_datetime, contributor_id, contributor_name, ingest_datetime, is_deleted,
+                display_name, extra_data
+            ) = row
+            contributor = contributors.get(contributor_id)
+            if contributor is None:
+                contributor = ArchiveContributor(contributor_name, contributor_id=contributor_id)
+                contributors[contributor_id] = contributor
+            snapshots.append(UserSnapshot(
+                website_id,
+                site_user_id,
+                contributor,
+                scan_datetime,
+                user_snapshot_id=snapshot_id,
+                ingest_datetime=ingest_datetime,
+                is_deleted=is_deleted,
+                display_name=display_name,
+                extra_data=extra_data
+            ))
+        if not snapshots:
             return None
-        (
-            user_id,
+        return User(
             website_id,
             site_user_id,
-            is_deleted,
-            first_scanned,
-            latest_update,
-            display_name,
-            extra_data,
-        ) = user_rows[0]
-        return cls(
-            user_id,
-            website_id,
-            site_user_id,
-            is_deleted,
-            first_scanned,
-            latest_update,
-            display_name,
-            extra_data,
+            snapshots
         )
 
 
-class UserUpdate:
+class UserSnapshot:
     def __init__(
             self,
             website_id: str,
             site_user_id: str,
-            update_time: datetime.datetime = None,
-            is_deleted: bool = False,
+            contributor: ArchiveContributor,
+            scan_datetime: datetime = None,
             *,
-            display_name: str = UNSET,
-            add_extra_data: Dict[str, Any] = UNSET,
+            user_snapshot_id: int = None,
+            ingest_datetime: datetime = None,
+            is_deleted: bool = False,
+            display_name: str = None,
+            extra_data: Dict[str, Any] = None,
     ):
         self.website_id = website_id
         self.site_user_id = site_user_id
-        self.update_time = update_time or datetime.datetime.now(datetime.timezone.utc)
-        self.update_time_set = update_time is not None
+        self.scan_datetime = scan_datetime or datetime.datetime.now(datetime.timezone.utc)
+        self.scan_datetime_set = scan_datetime is not None
+        self.contributor = contributor
+
+        self.user_snapshot_id = user_snapshot_id
+        self.ingest_datetime = ingest_datetime or datetime.datetime.now(datetime.timezone.utc)
         self.is_deleted = is_deleted
         self.display_name = display_name
-        self.add_extra_data = add_extra_data
+        self.extra_data = extra_data
 
-    def create_user(self, db: "Database") -> User:
-        display_name = unset_to_null(self.display_name)
-        extra_data = unset_to_null(self.add_extra_data)
+    def create_snapshot(self, db: "Database") -> None:
+        self.contributor.save(db)
         user_rows = db.insert(
-            "INSERT INTO users "
-            "(website_id, site_user_id, is_deleted, first_scanned, latest_update, display_name, extra_data) "
-            "VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING user_id", (
-                self.website_id, self.site_user_id, self.is_deleted, self.update_time, self.update_time,
-                display_name, extra_data
+            "WITH e AS( "
+            "INSERT INTO user_snapshots "
+            "(website_id, site_user_id, scan_datetime, archive_contributor_id, ingest_datatime, is_deleted, "
+            "display_name, extra_data) "
+            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s) "
+            "ON CONFLICT (website_id, site_user_id, scan_datetime, archive_contributor_id) DO NOTHING "
+            "RETURNING user_snapshot_id "
+            ") SELECT * FROM e "
+            "UNION "
+            "SELECT user_snapshot_id FROM user_snapshots "
+            "WHERE website_id = %s, site_user_id = %s, scan_datetime = %s, archive_contributor_id = %s, ",
+            (
+                self.website_id, self.site_user_id, self.scan_datetime, self.contributor.contributor_id,
+                self.ingest_datetime, self.is_deleted, self.display_name, json_to_db(self.extra_data),
+                self.website_id, self.site_user_id, self.scan_datetime, self.contributor.contributor_id
             )
         )
-        user_id = user_rows[0][0]
-        return User(
-            user_id,
-            self.website_id,
-            self.site_user_id,
-            self.is_deleted,
-            self.update_time,
-            self.update_time,
-            display_name,
-            extra_data
-        )
+        self.user_snapshot_id = user_rows[0][0]
 
-    def save(self, db: "Database") -> User:
-        user = User.from_database(db, self.website_id, self.site_user_id)
-        if user is not None:
-            user.add_update(self)
-            user.save(db)
-            return user
-        try:
-            return self.create_user(db)
-        except errors.lookup(UNIQUE_VIOLATION):
-            user = User.from_database(db, self.website_id, self.site_user_id)
-            if user is None:
-                raise ValueError("User existed, and then disappeared")
-            user.add_update(self)
-            user.save(db)
-            return user
-
+    def save(self, db: "Database") -> None:
+        if self.user_snapshot_id is None:
+            return self.create_snapshot(db)
