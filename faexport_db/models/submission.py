06bdce29ff73f66ec1086a4f76e484bc77ae9cf9
Deer-Spangle <deer@spangle.org.uk>
2022-07-12 10:03:55 +0100

    Starting to convert Submission to work with snapshots

--- faexport_db/models/submission.py
+++ faexport_db/models/submission.py
@@ -18,25 +18,17 @@ if TYPE_CHECKING:
 
 
 class Submission:
+    # TODO: re-implement to work from snapshots
     def __init__(
         self,
-        submission_id: int,
         website_id: str,
         site_submission_id: str,
-        is_deleted: bool,
-        first_scanned: datetime.datetime,
-        latest_update: datetime.datetime,
-        uploader: Optional[User],
-        title: Optional[str],
-        description: Optional[str],
-        datetime_posted: Optional[datetime.datetime],
-        extra_data: Optional[Dict[str, Any]],
-        keywords: SubmissionKeywordsList,
-        files: FileList,
+        snapshots: List["SubmissionSnapshot"],
     ):
-        self.submission_id = submission_id
         self.website_id = website_id
         self.site_submission_id = site_submission_id
+        self.snapshots = snapshots
+        # TODO: Convert to properties
         self.is_deleted = is_deleted
         self.first_scanned = first_scanned
         self.latest_update = latest_update
@@ -47,148 +39,69 @@ class Submission:
         self.datetime_posted = datetime_posted
         self.extra_data = extra_data
         self.keywords = keywords
-        self.keywords_update: SubmissionKeywordsListUpdate = UNSET
         self.files = files
-        self.files_update: FileListUpdate = UNSET
-        self.updated = False
-
-    def add_update(self, update: "SubmissionUpdate") -> None:
-        if update.update_time > self.latest_update:
-            self.updated = True  # At the very least, update time is being updated
-            self.latest_update = update.update_time
-            if update.is_deleted is not UNSET:
-                self.is_deleted = update.is_deleted
-            if update.uploader_update is not UNSET:
-                if self.uploader is not None:
-                    self.uploader.add_update(update.uploader_update)
-                else:
-                    self.uploader_create = update.uploader_update
-            if update.title is not UNSET:
-                self.title = update.title
-            if update.description is not UNSET:
-                self.description = update.description
-            if update.datetime_posted is not UNSET:
-                self.datetime_posted = update.datetime_posted
-            if update.add_extra_data is not UNSET:
-                self.extra_data = merge_dicts(self.extra_data, update.add_extra_data)
-            if update.ordered_keywords is not UNSET:
-                self.keywords_update = SubmissionKeywordsListUpdate.from_ordered_keywords(update.ordered_keywords)
-            if update.unordered_keywords is not UNSET:
-                self.keywords_update = SubmissionKeywordsListUpdate.from_unordered_keywords(update.unordered_keywords)
-            if update.files is not UNSET:
-                self.files_update = update.files
-            return
-        # If it's an older update, we can still update some things if they're unset
-        if self.title is None and update.title is not UNSET:
-            self.updated = self.updated or (self.title != update.title)
-            self.title = update.title
-        if self.description is None and update.description is not UNSET:
-            self.updated = self.updated or (self.description != update.description)
-            self.description = update.description
-        if self.datetime_posted is None and update.datetime_posted is not UNSET:
-            self.updated = self.updated or (
-                self.datetime_posted != update.datetime_posted
-            )
-            self.datetime_posted = update.datetime_posted
-        if update.add_extra_data is not UNSET:
-            new_extra_data = merge_dicts(update.add_extra_data, self.extra_data)
-            self.updated = self.updated or (self.extra_data != new_extra_data)
-            self.extra_data = new_extra_data
-        # Update uploader
-        if update.uploader_update is not UNSET:
-            if self.uploader is None:
-                self.uploader_create = update.uploader_update
-            else:
-                self.uploader.add_update(update.uploader_update)
-        # Update keywords
-        if self.keywords is None and update.ordered_keywords is not UNSET:
-            self.keywords_update = SubmissionKeywordsListUpdate.from_ordered_keywords(update.ordered_keywords)
-        if self.keywords is None and update.unordered_keywords is not UNSET:
-            self.keywords_update = SubmissionKeywordsListUpdate.from_unordered_keywords(update.unordered_keywords)
-        # Update files
-        if update.files is not UNSET:
-            self.files.add_update(update.files)
-        # Update first scanned, if this update is older
-        if self.first_scanned > update.update_time:
-            self.updated = True
-            self.first_scanned = update.update_time
-
-    def save(self, db: "Database") -> None:
-        if self.updated:
-            db.update(
-                "UPDATE submissions "
-                "SET is_deleted = %s, first_scanned = %s, latest_update = %s, uploader_id = %s, title = %s, "
-                "description = %s, datetime_posted = %s, extra_data = %s "
-                "WHERE submission_id = %s",
-                (
-                    self.is_deleted,
-                    self.first_scanned,
-                    self.latest_update,
-                    self.uploader.user_id if self.uploader else None,
-                    self.title,
-                    self.description,
-                    self.datetime_posted,
-                    json_to_db(self.extra_data),
-                    self.submission_id,
-                ),
-            )
-        # Update uploader
-        if self.uploader is not None and self.uploader.updated:
-            self.uploader.save(db)
-        if self.uploader_create is not UNSET:
-            self.uploader = self.uploader_create.create_user(db)
-        # Update keywords
-        if self.keywords_update is not UNSET:
-            self.keywords = self.keywords_update.save(db, self.submission_id)
-        # Update files
-        if self.files.updated:
-            self.files.save(db)
-        if self.files_update is not UNSET:
-            self.files = self.files_update.create(db, self.submission_id)
 
     @classmethod
     def from_database(
         cls, db: "Database", website_id: str, site_submission_id: str
     ) -> Optional["Submission"]:
-        sub_rows = db.select(
-            "SELECT submission_id, is_deleted, first_scanned, latest_update, uploader_id, title, description, "
-            "datetime_posted, extra_data "
-            "FROM submissions "
-            "WHERE website_id = %s AND site_submission_id = %s",
-            (website_id, site_submission_id),
+        snapshot_rows = db.select(
+            "SELECT s.submission_snapshot_id, s.scan_datetime, s.archive_contributor_id, a.name as contributor_name, "
+            "s.ingest_datetime, s.uploader_site_user_id, s.is_deleted, s.title, s.description, s.datetime_posted, "
+            "s.extra_data "
+            "FROM submission_snapshots s "
+            "LEFT JOIN archive_contributors a ON s.archive_contributor_id = a.contributor_id "
+            "WHERE website_id = %s AND site_submission_id = %s"
         )
-        if not sub_rows:
+        snapshots = []
+        contributors = {}
+        for row in snapshot_rows:
+            (
+                submission_snapshot_id, scan_datetime, contributor_id, contributor_name, ingest_datetime,
+                uploader_site_user_id, is_deleted, title, description, datetime_posted, extra_data
+            ) = row
+            contributor = contributors.get(contributor_id)
+            if contributor is None:
+                contributor = ArchiveContributor(contributor_name, contributor_id=contributor_id)
+                contributors[contributor_id] = contributor
+            # Load keywords
+            keyword_rows = db.select(
+                "SELECT keyword_id, keyword, ordinal "
+                "FROM submission_snapshot_keywords "
+                "WHERE submission_snapshot_id = %s",
+                (submission_snapshot_id,)
+            )
+            keywords = []
+            for keyword_row in keyword_rows:
+                keyword_id, keyword, ordinal = keyword_row
+                keywords.append(SubmissionKeyword(
+                    keyword,
+                    submission_snapshot_id=submission_snapshot_id,
+                    keyword_id=keyword_id,
+                    ordinal=ordinal
+                ))
+            # TODO: Load files
+            snapshots.append(SubmissionSnapshot(
+                website_id,
+                site_submission_id,
+                contributor,
+                scan_datetime,
+                submission_snapshot_id=submission_snapshot_id,
+                ingest_datetime=ingest_datetime,
+                uploader_site_user_id=uploader_site_user_id,
+                is_deleted=is_deleted,
+                title=title,
+                description=description,
+                datetime_posted=datetime_posted,
+                extra_data=extra_data,
+                keywords=keywords
+            ))
+        if not snapshots:
             return None
-        (
-            sub_id,
-            is_deleted,
-            first_scanned,
-            latest_update,
-            uploader_id,
-            title,
-            description,
-            datetime_posted,
-            extra_data,
-        ) = sub_rows[0]
-        uploader = None
-        if uploader_id is not None:
-            uploader = User.from_database_by_user_id(db, uploader_id)
-        keywords = SubmissionKeywordsList.from_database(db, sub_id)
-        files = FileList.from_database(db, sub_id)
-        return cls(
-            sub_id,
+        return Submission(
             website_id,
             site_submission_id,
-            is_deleted,
-            first_scanned,
-            latest_update,
-            uploader,
-            title,
-            description,
-            datetime_posted,
-            extra_data,
-            keywords,
-            files
+            snapshots
         )
 
 
