cd65e27cb343216fde4aa0f09fe78de51e3ea6b3
Deer-Spangle <deer@spangle.org.uk>
2022-07-26 23:18:49 +0100

    Converting Foxoblue userlist ingestion script to use a subclass of IngestionJob

--- scripts/ingest/foxoblue_userlist/main.py
+++ scripts/ingest/foxoblue_userlist/main.py
@@ -1,16 +1,20 @@
 import datetime
 import json
 import csv
-from typing import Optional, List, Dict
+from pathlib import Path
+from typing import Optional, Iterator
 
 import dateutil.parser
 import psycopg2
+
+from faexport_db.ingest_formats.base import FormatResponse
 from faexport_db.models.archive_contributor import ArchiveContributor
 from faexport_db.models.user import UserSnapshot
 import tqdm
 
 from faexport_db.db import Database
 from scripts.ingest.fa_indexer.main import setup_initial_data
+from scripts.ingest.ingestion_job import IngestionJob, RowType, cache_in_file
 
 CSV_LOCATION = "./dump/foxoblue_userlist/data-1642685938898.csv"
 SITE_ID = "fa"
@@ -18,54 +22,67 @@ DATA_DATE = datetime.datetime(2022, 1, 20, 13, 38, 58, tzinfo=datetime.timezone.
 CONTRIBUTOR = ArchiveContributor("Foxo//Blue FA user list export")
 
 
-def import_row(row: Dict[str, str], db: Database) -> Optional[UserSnapshot]:
-    username, updated_at, error = row.values()
-    if error == "unknown_user":
-        return None
-    scan_datetime = csv_earliest_date()
-    if updated_at != "NULL":
-        scan_datetime = dateutil.parser.parse(updated_at)
-    is_deleted = error != "NULL"
-    extra_data = None
-    if is_deleted:
-        extra_data = {
-            "deletion_type": error
-        }
-    snapshot = UserSnapshot(
-        SITE_ID,
-        username,
-        CONTRIBUTOR,
-        scan_datetime,
-        is_deleted=is_deleted,
-        extra_data=extra_data,
-    )
-    snapshot.save(db)
-    return snapshot
+class FoxoBlueUserListIngestor(IngestionJob):
 
+    def __init__(self):
+        super().__init__()
+        self.csv_location = CSV_LOCATION
+        self.row_count_file = Path(__file__) / "row_count.txt"
+        self.earliest_date_file = Path(__file__) / "earliest_date.txt"
 
-def csv_row_count() -> int:
-    with open(CSV_LOCATION, "r", encoding="utf-8") as file:
-        reader = csv.reader(file)
-        return sum(1 for _ in tqdm.tqdm(reader))
+    def _count_rows_in_csv(self) -> int:
+        with open(self.csv_location, "r", encoding="utf-8") as file:
+            reader = csv.reader(file)
+            return sum(1 for _ in tqdm.tqdm(reader, desc="Counting rows"))
 
+    def _earliest_date_in_csv(self) -> datetime.datetime:
+        earliest = "zzz"
+        with open(self.csv_location, "r", encoding="utf-8") as file:
+            reader = csv.reader(file)
+            for line in reader:
+                if line[5]:
+                    earliest = min(earliest, line[5])
+        return dateutil.parser.parse(earliest)
 
-def csv_earliest_date() -> datetime.datetime:
-    # TODO: caching
-    earliest = "zzz"
-    with open(CSV_LOCATION, "r", encoding="utf-8") as file:
-        reader = csv.reader(file)
-        for line in reader:
-            if line[5]:
-                earliest = min(earliest, line[5])
-    return dateutil.parser.parse(earliest)
+    def row_count(self) -> Optional[int]:
+        return int(cache_in_file(self.row_count_file, lambda: str(self._count_rows_in_csv())))
 
+    def earliest_date(self) -> datetime.datetime:
+        return dateutil.parser.parse(
+            cache_in_file(
+                self.earliest_date_file,
+                lambda: self._earliest_date_in_csv().isoformat()
+            )
+        )
 
-def ingest_csv(db: Database) -> None:
-    row_count = csv_row_count()
-    with open(CSV_LOCATION, "r", encoding="utf-8") as file:
-        reader = csv.DictReader(file)
-        for row in tqdm.tqdm(reader, total=row_count):
-            import_row(dict(row), db)
+    def convert_row(self, row: RowType) -> FormatResponse:
+        username, updated_at, error = row.values()
+        if error == "unknown_user":
+            return FormatResponse()
+        scan_datetime = self.earliest_date()
+        if updated_at != "NULL":
+            scan_datetime = dateutil.parser.parse(updated_at)
+        is_deleted = error != "NULL"
+        extra_data = None
+        if is_deleted:
+            extra_data = {
+                "deletion_type": error
+            }
+        snapshot = UserSnapshot(
+            SITE_ID,
+            username,
+            CONTRIBUTOR,
+            scan_datetime,
+            is_deleted=is_deleted,
+            extra_data=extra_data,
+        )
+        return FormatResponse(user_snapshots=[snapshot])
+
+    def iterate_rows(self) -> Iterator[RowType]:
+        with open(self.csv_location, "r", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                yield row
 
 
 if __name__ == "__main__":
@@ -77,4 +94,5 @@ if __name__ == "__main__":
     db_obj = Database(db_conn)
     setup_initial_data(db_obj, CONTRIBUTOR)
 
-    ingest_csv(db_obj)
+    ingestion_job = FoxoBlueUserListIngestor()
+    ingestion_job.ingest_data(db_obj)
